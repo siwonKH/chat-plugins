ca6cc975de81e7f4436a31df158ceb758bad51f4
siwonKH <kanghyunkim313@gmail.com>
2023-05-31 14:21:14 +0900

    rewrite and add version

--- dialog.js
+++ dialog.js
@@ -1,9 +1,31 @@
+const __pluginId__ = 'dialog'
+const __dialogVersion__ = 'v0.1'
+
 window.l = async () => {
+    const dialogDiv = await customShowDialog()
     for (;;) {
-        const msg = await customShowDialog()
+        const msg = await waitInput(dialogDiv)
         if (!msg) break
         await chat([msg])
     }
+}
+
+function waitInput(dialogDiv) {
+    return new Promise((resolve) => {
+        const chatInput = dialogDiv.querySelector('#chatInput')
+        chatInput.focus()
+        chatInput.addEventListener('keydown', handleKeyDown)
+        function handleKeyDown(event) {
+            if (event.key === 'Enter') {
+                event.preventDefault()
+                if (!chatInput.value) {
+                    chatInput.removeEventListener('keydown', handleKeyDown)
+                    window.__dialogutils.closeDialog(dialogDiv)
+                }
+                resolve(chatInput.value)
+            }
+        }
+    })
 }
 
 function customShowDialog() {
@@ -12,17 +34,8 @@ function customShowDialog() {
             <input type="text" id="chatInput">
         `
         const dialogDiv = window.__dialogutils.showDialog('Enter Chat', bodyElement)
-
-        const chatInput = dialogDiv.querySelector('#chatInput')
-        chatInput.focus()
-        chatInput.addEventListener('keydown', handleKeyDown)
-        function handleKeyDown(event) {
-            if (event.key === 'Enter') {
-                event.preventDefault()
-                document.body.removeChild(dialogDiv)
-                chatInput.removeEventListener('keydown', handleKeyDown)
-                resolve(chatInput.value)
-            }
-        }
+        resolve(dialogDiv)
     })
 }
+
+console.log(__pluginId__, __dialogVersion__)
